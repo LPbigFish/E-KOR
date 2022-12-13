08974e96e22652294fdd0c2dcc1dd154aff0dd9c
BuildTools <unconfigured@null.spigotmc.org>
2022-12-13 21:57:45 +0100

    Relocated some important values

--- src/main/java/org/LPbigFish/Components/Blockchain.java
+++ src/main/java/org/LPbigFish/Components/Blockchain.java
@@ -2,9 +2,7 @@ package org.LPbigFish.Components;
 
 import org.LPbigFish.Security.Hasher;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -12,17 +10,13 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Blockchain {
-    private static long difficulty = 500000;
-    private static BigDecimal target = new BigDecimal(new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16));
-
-    private static final BigInteger TARGET_MAX = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
     private static long avgNonce = 20000000L;
     private final List<Block> chain = new ArrayList<>();
 
-
+    public static boolean synced = true;
 
     public Blockchain() {
-        target = target.divide(new BigDecimal(difficulty), 0, RoundingMode.HALF_UP);
+        Values.init();
         addBlock(mine(new SubBlock(0, System.currentTimeMillis() / 1000L, "0000000000000000000000000000000000000000000000000000000000000000", "0", "Genesis Block", 0)));
         run();
     }
@@ -34,12 +28,12 @@ public class Blockchain {
     private void addBlock(Block block) {
         block.printBlock();
         chain.add(block);
-        adjustDiff();
-        System.out.println("Work Value: " + getWorkValue()); //73967685 na indexu 10
+        Values.adjustDiff(chain);
+        System.out.println("Work Value: " + Values.getWorkValue(chain)); //73967685 na indexu 10
     }
 
     public void run() {
-        while (true) {
+        while (synced) {
             addBlock(mine(new SubBlock(getLatestBlock().index() + 1, System.currentTimeMillis() / 1000L, getLatestBlock().hash(), "0000000000000000000000000000000000000000000000000000000000000000", Math.random() + "", 0)));
         }
 
@@ -54,27 +48,6 @@ public class Blockchain {
             }
         }
         return true;
-    }
-
-    private void adjustDiff() {
-        if (chain.size() % 20 == 0) {
-            Block latestBlock = getLatestBlock();
-            Block previousBlock = chain.get(chain.size() - 20);
-
-            double timeDiff = Math.round(((double) (latestBlock.timestamp() - previousBlock.timestamp()) / (50 * 20)) * 100.0) / 100.0;
-            difficulty /= timeDiff;
-            target = new BigDecimal(TARGET_MAX.divide(BigInteger.valueOf(difficulty)));
-            System.out.println("Difficulty was multiplied by: " + timeDiff);
-            adjustAvgNonce();
-        }
-         /*if (chain.size() > 1) {
-            Block latestBlock = getLatestBlock();
-            Block previousBlock = chain.get(chain.size() - 2);
-            long prevDiff = TARGET_MAX.divide(new BigInteger(previousBlock.target())).longValue();
-
-            difficulty = (long) (prevDiff + Math.round(prevDiff / 2048d) * Math.max(1 - (Math.round((latestBlock.timestamp() - previousBlock.timestamp())) / 20), -99) + Math.floor(2 ^ Math.round(latestBlock.index() / 100000d) - 2));
-            target = new BigDecimal(TARGET_MAX.divide(BigInteger.valueOf(difficulty)));
-        }*/
     }
 
     private void adjustAvgNonce() {
@@ -97,18 +70,18 @@ public class Blockchain {
         int cores = Runtime.getRuntime().availableProcessors() - 2;
         ExecutorService executor = Executors.newFixedThreadPool(cores);
         List<Future<Block>> futures = new ArrayList<>();
-        long startNonce = Math.round(avgNonce / (double) cores);
+        //long startNonce = Math.round(avgNonce / (double) cores);
         /*for (int i = 0; i < cores; i++) {
             Mine mine;
             if (i == cores - 1) {
-                mine = new Mine(block, difficulty.toBigInteger(), i * startNonce, Long.MAX_VALUE);
+                mine = new Mine(block, Values.getTarget().toBigInteger().divide(BigInteger.valueOf(16)), i * startNonce, Long.MAX_VALUE);
             } else {
-                mine = new Mine(block, difficulty.toBigInteger(), i * startNonce, (i + 1) * startNonce);
+                mine = new Mine(block, Values.getTarget().toBigInteger().divide(BigInteger.valueOf(16)), i * startNonce, (i + 1) * startNonce);
             }
             futures.add(executor.submit(mine));
         }*/
 
-        futures.add(executor.submit(new Mine(block, target.toBigInteger(), 0, Long.MAX_VALUE)));
+        futures.add(executor.submit(new Mine(block, Values.getTarget().toBigInteger().divide(BigInteger.valueOf(cores)), 0, Long.MAX_VALUE)));
         while (newBlock == null) {
             for (Future<Block> future : futures) {
                 if (future.isDone()) {
@@ -128,17 +101,6 @@ public class Blockchain {
         if(chain.size() != 0)
             newBlock = new Block(newBlock.index(), newBlock.timestamp(), newBlock.previousHash(), newBlock.hash(), newBlock.data(), newBlock.nonce(), newBlock.target(), newBlock.timestamp() - getLatestBlock().timestamp());
         return newBlock;
-    }
-
-    public BigInteger getWorkValue() {
-        BigDecimal work = new BigDecimal(new BigInteger("0"));
-        BigInteger maxHash = new BigInteger("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16);
-        for (Block block: chain) {
-            BigInteger temp_target = (new BigInteger(block.target())).add(BigInteger.ONE);
-            work = work.add(new BigDecimal(maxHash).divide(new BigDecimal(temp_target), RoundingMode.HALF_UP));
-        }
-
-        return work.toBigInteger();
     }
 
     public void printTheChain() {
---
+++ src/main/java/org/LPbigFish/Components/Values.java
@@ -0,0 +1,59 @@
+package org.LPbigFish.Components;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.List;
+
+public class Values {
+    private static BigDecimal target = new BigDecimal(new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16));
+
+    private static final BigInteger TARGET_MAX = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
+
+    private static long difficulty = 500000;
+
+    public static void init() {
+        target = target.divide(new BigDecimal(difficulty), 0, RoundingMode.HALF_UP);
+    }
+
+    public static void adjustDiff(List<Block> chain) {
+        if (chain.size() % 20 == 0) {
+            Block latestBlock = chain.get(chain.size() - 1);
+            Block previousBlock = chain.get(chain.size() - 20);
+
+            double timeDiff = Math.round(((double) (latestBlock.timestamp() - previousBlock.timestamp()) / (50 * 20)) * 100.0) / 100.0;
+            if (timeDiff > 8)
+                difficulty /= 8;
+            else difficulty /= Math.max(timeDiff, 0.01);
+            target = new BigDecimal(TARGET_MAX.divide(BigInteger.valueOf(difficulty)));
+            System.out.println("Difficulty was multiplied by: " + timeDiff);
+        }
+         /*if (chain.size() > 1) {
+            Block latestBlock = getLatestBlock();
+            Block previousBlock = chain.get(chain.size() - 2);
+            long prevDiff = TARGET_MAX.divide(new BigInteger(previousBlock.target())).longValue();
+
+            difficulty = (long) (prevDiff + Math.round(prevDiff / 2048d) * Math.max(1 - (Math.round((latestBlock.timestamp() - previousBlock.timestamp())) / 20), -99) + Math.floor(2 ^ Math.round(latestBlock.index() / 100000d) - 2));
+            target = new BigDecimal(TARGET_MAX.divide(BigInteger.valueOf(difficulty)));
+        }*/
+    }
+
+    public static BigInteger getWorkValue(List<Block> chain) {
+        BigDecimal work = new BigDecimal(new BigInteger("0"));
+        BigInteger maxHash = new BigInteger("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16);
+        for (Block block: chain) {
+            BigInteger temp_target = (new BigInteger(block.target())).add(BigInteger.ONE);
+            work = work.add(new BigDecimal(maxHash).divide(new BigDecimal(temp_target), RoundingMode.HALF_UP));
+        }
+
+        return work.toBigInteger();
+    }
+
+    public static BigDecimal getTarget() {
+        return target;
+    }
+
+    public static long getDifficulty() {
+        return difficulty;
+    }
+}
